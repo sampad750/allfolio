4b02ef3de501c87022b940bb9c7d34559c51c902
Sampad Debnath <sampad750@gmail.com>
2022-05-28 16:41:22 +0600

    28_5_2022

--- page-full-screen-slider.php
+++ page-full-screen-slider.php
@@ -1,17 +1,20 @@
 <?php
+
 /**
  * Template Name: Allfolio Full Screen Slider
  */
 
 get_header();
-    ?>
-    <!-- Full Page Slider -->
-    <div id="fullpage" class="full-page-slide">
-        <?php        
-        while ( have_posts() ) : the_post();
+?>
+<!-- Full Page Slider -->
+<div id="fullpage" class="full-page-slide">
+    <div class="container">
+        <?php
+        while (have_posts()) : the_post();
             the_content();
         endwhile;
         ?>
     </div>
-    <?php
+</div>
+<?php
 get_footer('empty');
